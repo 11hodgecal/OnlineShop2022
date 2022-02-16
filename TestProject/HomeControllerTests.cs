a7efea17f5c227035212b3eb658fe57cb69bdc7f
HOD16272577 <HOD16272577@C-010063>
2022-02-16 14:33:21 +0000

    Added Home Controller Test

---
+++ TestProject/HomeControllerTests.cs
@@ -0,0 +1,63 @@
+using System;
+using Xunit;
+using OnlineShop2022;
+using Microsoft.Extensions.Logging;
+using OnlineShop2022.Controllers;
+using OnlineShop2022.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestProject
+{
+    public class HomeControllerTests
+    {
+        private ILogger<HomeController> _logger;
+        private AppDbContext _db;
+
+        private void CreateMocDB()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>().UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+            _db = new AppDbContext(options);
+            _db.Database.EnsureCreated();
+        }
+        [Fact]
+        public void HomeControllerIndexNotNull()
+        {
+            //Arrange
+            CreateMocDB();
+            var home = new HomeController(_logger, _db);
+
+            //Act
+            var result = home.Index();
+            //Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void privacyNotNull()
+        {
+            //Arrange
+            CreateMocDB();
+            var home = new HomeController(_logger, _db);
+
+            //Act
+            var result = home.Privacy();
+            //Assert
+            Assert.NotNull(result);
+
+        }
+
+        [Fact]
+        public void ProductsNotNull()
+        {
+            //Arrange
+            CreateMocDB();
+            var home = new HomeController(_logger, _db);
+
+            //Act
+            var result = home.Products("dfadfa31141fadfa");
+            //Assert
+            Assert.NotNull(result);
+
+        }
+    }
+}
---
+++ TestProject/ProductControllerTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Xunit;
+using OnlineShop2022;
+using Microsoft.Extensions.Logging;
+using OnlineShop2022.Controllers;
+using OnlineShop2022.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using OnlineShop2022.Areas.Admin;
+using OnlineShop2022.Models;
+using OnlineShop2022.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestProject
+{
+    public class ProductControllerTests
+    {
+        private ILogger<HomeController> _logger;
+        private AppDbContext _db;
+        private IWebHostEnvironment _webHostEnviroment;
+
+        private void CreateMocDB()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>().UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+            _db = new AppDbContext(options);
+            _db.Database.EnsureCreated();
+        }
+        [Fact]
+        public async void ProductUpdateReturnNull()
+        {
+            CreateMocDB();
+            var _images = new Images(_webHostEnviroment);
+            ProductController controller = new ProductController(_db,_webHostEnviroment, _images);
+            ProductViewModel product = new ProductViewModel();
+            product.Product = new ProductModel();
+            product.Product.Description = "DFADFADFA";
+            product.Product.Id = 1;
+            //act 
+            var result = await controller.Update(2, product) as NotFoundResult;
+            
+            //assert
+            Assert.Equal("404", result.StatusCode.ToString());
+            
+        }
+    }
+}
