5e1df1829ce6e715a10e67d16cee833e732898be
callu <callu@GRIMREAPERIII>
2022-03-18 14:03:09 +0000

    Made it so the customer order view items appear by time decending and filtered on whether the order was made within  2 months or a Year. changed the way certain parts of the page appears

--- OnlineShop2022/Controllers/OrderViewController.cs
+++ OnlineShop2022/Controllers/OrderViewController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using OnlineShop2022.Data;
 using OnlineShop2022.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -24,30 +25,65 @@ namespace OnlineShop2022.Controllers
 
         public async Task<IActionResult> Index()
         {
+            
+            //creates a list of order view models to be populated
+            var list = new List<OrderViewModel>();
+
+            GetUsersOrders(list);
+
+            //returns the order
+            var filter = "last 60 days";
+            ViewBag.filterContent = filter;
+            return View(list.OrderByDescending(s=>s.ordertime).Where(s=> s.ordertime > DateTime.Now.AddDays(-60)).ToList());
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(string filter)
+        {
+            //creates a list of order view models to be populated
+            var list = new List<OrderViewModel>();
+
+            GetUsersOrders(list);
+            //returns the order
+
+            ViewBag.filterContent = filter;
+
+            if (filter == "last 60 days")
+            {
+                return View(list.OrderByDescending(s => s.ordertime).Where(s => s.ordertime > DateTime.Now.AddDays(-60)).ToList());
+            }
+
+            
+            return View(list.OrderByDescending(s => s.ordertime).Where(s => s.ordertime > DateTime.Now.AddYears(-1)).ToList());
+
+            
+        }
+
+        
+        public async void GetUsersOrders(List<OrderViewModel> UserOrders)
+        {
             //gets the current user
             var user = _signInManager.Context.User;
             var UserID = _userManager.GetUserAsync(user).Result.Id;
             //gets the current users orders
             var items = await _db.Orders.Where(s => s.UserID == UserID).ToListAsync();
 
-            //creates a list of order view models to be populated
-            var list = new List<OrderViewModel>();
-
             foreach (var item in items)
             {
                 //each order will create a new view model which takes the order id and order time 
-                var order = new OrderViewModel() { id = item.OrderId, ordertime = item.OrderPlaced };
+                var order = new OrderViewModel() { id = item.OrderId, ordertime = item.OrderPlaced};
 
                 //gets all the order details attached to the current order
                 var orderDetails = await _db.OrderDetails.Where(s => s.OrderId == item.OrderId).ToListAsync();
 
                 //creates a list of product view models to be populated
                 var orderItems = new List<OrderProductViewModel>();
-                
-                foreach (var orderDetail in orderDetails) {
+
+                foreach (var orderDetail in orderDetails)
+                {
 
                     //each order detail will create a product view model which will take the order detail id and the quantity of the item
-                    var orderitem = new OrderProductViewModel() { Id = orderDetail.OrderDetailId, quantity = orderDetail.Amount};
+                    var orderitem = new OrderProductViewModel() { Id = orderDetail.OrderDetailId, quantity = orderDetail.Amount };
 
                     //gets the product attach to the detail
                     var product = _db.Products.Where(s => s.Id == orderDetail.ProductId).FirstOrDefault();
@@ -64,10 +100,8 @@ namespace OnlineShop2022.Controllers
                 //attach the orderitems to the order view model
                 order.products = orderItems;
                 //adds the order to the order list
-                list.Add(order);
+                UserOrders.Add(order);
             }
-            //returns the order
-            return View(list);
         }
     }
 }
